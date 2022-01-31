d7bfbb672a75ef50c7895ebd54c718b340e44e79
LC-OCSB <97174090+Clarke47@users.noreply.github.com>
2022-01-31 18:54:26 +0000

    u4a4 counting

---
+++ Q1Counting.py
@@ -0,0 +1,13 @@
+#NAME OF AUTHOR: Liam Clarke
+#NAME OF PROGRAM: counting
+#DATE OF CREATION: 23-01-22
+#PURPOSE OF PROGRAM: counts back freom a number
+#opening text
+print("Welcome To counting")
+#inputs"
+number1 = int(input("Please type a big number: "))
+number2 = int(input("what number would you like to coun back by?: "))
+#Calculating and output
+while number1 >= 0:
+  print(number1)
+  number1 = number1 - number2
